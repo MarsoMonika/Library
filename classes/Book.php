2304ce7dcccb18a8d46d4f5abbce7543d8adf52a
Marsó Mónika <marso.monika04@gmail.com>
2025-06-16 21:04:06 +0200

    Basic CRUD functions for BookRepository and adding id attribute to Book class

--- classes/Book.php
+++ classes/Book.php
@@ -6,13 +6,40 @@ class Book
     private string $author;
     private int $publishYear;
     private bool $isAvailable;
+    private ?int $id;
 
-    public function __construct(string $title, string $author, int $publishYear, bool $isAvailable)
+    public function __construct(string $title, string $author, int $publishYear, bool $isAvailable, ?int $id = null)
     {
         $this->title = $title;
         $this->author = $author;
         $this->publishYear = $publishYear;
         $this->isAvailable = $isAvailable;
+        $this->id = $id;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function getAuthor(): string
+    {
+        return $this->author;
+    }
+
+    public function getPublishYear(): int
+    {
+        return $this->publishYear;
+    }
+
+    public function isAvailable(): bool
+    {
+        return $this->isAvailable;
     }
 
 }
--- classes/BookRepository.php
+++ classes/BookRepository.php
@@ -1,1 +1,82 @@
-git <?php
+<?php
+
+require_once 'Book.php';
+require_once 'Database.php';
+
+class BookRepository
+{
+    private PDO $conn;
+
+    public function __construct(Database $db)
+    {
+        $this->conn = $db->connect();
+    }
+
+    public function add(Book $book): bool
+    {
+        $stmt = $this->conn->prepare("INSERT INTO books (Title, Author, PublishYear, IsAvailable) VALUES (?,?,?,?)"
+        );
+        return $stmt->execute([
+            $book->getTitle(),
+            $book->getAuthor(),
+            $book->getPublishYear(),
+            (int)$book->isAvailable()
+        ]);
+    }
+
+    public function getAll(): array
+    {
+        $stmt = $this->conn->prepare("SELECT * FROM books");
+        $stmt->execute();
+        $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $books = [];
+
+        foreach ($rows as $row) {
+            $books[] = new Book(
+                $row['Title'],
+                $row['Author'],
+                (int)$row['PublishYear'],
+                (bool)$row['IsAvailable'],
+                (int)$row['Id']
+            );
+        }
+        return $books;
+    }
+
+    public function getById(int $id): ?Book
+    {
+        $stmt = $this->conn->prepare("SELECT * FROM books WHERE Id = ?");
+        $stmt->execute([$id]);
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if (!$row) {
+            return null;
+        }
+        return new Book(
+            $row['Title'],
+            $row['Author'],
+            (int)$row['PublishYear'],
+            (int)$row['IsAvailable'],
+            (int)$row['Id']
+        );
+    }
+
+    public function update(Book $book): bool
+    {
+        $stmt = $this->conn->prepare("UPDATE books SET Title = ?, Author = ?, PublishYear = ?, IsAvailable = ? WHERE Id = ?"
+        );
+        return $stmt->execute([
+            $book->getTitle(),
+            $book->getAuthor(),
+            $book->getPublishYear(),
+            (int)$book->isAvailable(),
+            $book->getId()
+        ]);
+    }
+
+    public function delete(int $id): bool
+    {
+        $stmt = $this->conn->prepare("DELETE FROM books WHERE Id = ?");
+        return $stmt->execute([$id]);
+    }
+}
